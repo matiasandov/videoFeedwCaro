8f9d6bdd68be2344ecc1e170bd9829055eb56151
Matt <a01422885@itesm.mx>
2022-05-16 11:37:34 -0500

    nueva color palette

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,18 +12,23 @@ export default function Home({data}) {
         return data.map((renderItem) => {
             //lo de css de classname es lo que hace que el feed se ve con 3 cols y tambien el archivo home
             return (
-                <div className='flex justify-center items-center flex-col mt-4' key={renderItem.name}>
+                <div className='flex justify-center items-center text-white flex-col mt-4' key={renderItem.name}>
                     <div className='category-outer-circle'>
                         <div className='category-inner-circle flex justify-center items-center'>
-                            <i className="material-icons text-4xl">{renderItem.iconName}</i>
+                            <i className="material-icons text-4xl text-white">{renderItem.iconName}</i>
                         </div>
 
                     </div>
 
-                    <div className='font-bold'>{renderItem.name}</div>
-                    <div className='font-bold'>{renderItem.duration}</div>
+                    {/* hacer que se vean separados las cosas */}
+                    <div className=' '>
+                        <div className='font-bold'>{renderItem.name} </div>
+                       
+                        <div className='font-bold'>{renderItem.duration}</div>
+                    </div>
+                    
                    
-                    <div className='row'>
+                    <div className='flex justify-center items-center'>
                         <i className="material-icons text-4xl">{renderItem.stars}</i>
                         <p className='font-bold'>{renderItem.rating}</p>
                     </div>
@@ -40,9 +45,9 @@ export default function Home({data}) {
 
 
     return (
-        <div>
+        <div className='main-background'>
             
-            <div className="header flex justify-center items-center text-3xl font-bold" > SYNCALL </div>
+            <div className="header flex justify-center items-center text-3xl font-bold syncall-header" > SYNCALL </div>
             <div className='grid grid-cols-3'>
                 {renderCategoryItem()}
             </div>
